6efea8178101bac45168ed2d3625adbb07782f06
maltreya <amariakupko@gmail.com>
2023-02-27 15:36:09 +0100

    finish CRUD without DB

--- Lesson21_Starter.CRUD_App1/src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
+++ Lesson21_Starter.CRUD_App1/src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -41,7 +41,21 @@ public class PeopleController {
     public String create(@ModelAttribute("person") Person person){
         personDAO.save(person);
         return "redirect:/people";
-
+    }
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id){
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+    @DeleteMapping("{id}")
+    public String delete(@PathVariable("id") int id){
+        personDAO.delete(id);
+        return "redirect:/people";
     }
 
 }
